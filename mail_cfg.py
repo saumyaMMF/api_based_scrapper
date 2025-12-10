883feb0df8d3cafaa24c25f379d2d6b27c2fdfec
Shaily Litoriya <shaily.litoriyammf@gmail.com>
2025-12-10 04:28:53 -0500

    mail config updated

---
+++ mail_cfg.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.getenv("user")
+pw = os.getenv("pw")
+receiver = os.getenv("receiver")
+receiver_test = os.getenv("receiver_test")
+
+
+
+
